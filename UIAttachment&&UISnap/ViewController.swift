9aa9c4c804796b31d18c803b56167180df7822ae
Топорков Максим <toporkovmaksim7@gmail.com>
2020-01-17 14:05:53 +0700

    Initial Commit

---
+++ UIAttachment&&UISnap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIAttachment&&UISnap
+//
+//  Created by max on 17.01.2020.
+//  Copyright © 2020 max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
